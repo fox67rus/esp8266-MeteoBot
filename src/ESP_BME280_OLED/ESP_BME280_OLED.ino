26bc2007f6f1517eeadcef393daa5de8b34f6cde
Elena <fox67rus@mail.ru>
2023-02-17 14:04:20 +0300

    Отключен дисплей (закомментированы строки), новый шаблон отображения, высота над уровнем моря 212 м

--- src/ESP_BME280_OLED/ESP_BME280_OLED.ino
+++ src/ESP_BME280_OLED/ESP_BME280_OLED.ino
@@ -1,72 +1,91 @@
-#include <ESP8266WiFi.h>                                // Подключаем библиотеку ESP8266WiFi
-#include <Wire.h>                                       // Подключаем библиотеку Wire
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_BME280.h>                            // Подключаем библиотеку Adafruit_BME280
-#include <Adafruit_Sensor.h>                            // Подключаем библиотеку Adafruit_Sensor
-#include "config.h"
+// #include <ESP8266WiFi.h>
+// #include <SPI.h>
+#include <ESP8266WebServer.h>
+#include <Wire.h>
 
+//#include <Adafruit_GFX.h>
+//#include <Adafruit_SSD1306.h>
+
+#include <Adafruit_BME280.h>                            // Подключаем библиотеку для работы с датчиком
+#include <Adafruit_Sensor.h>                            // Подключаем библиотеку для работы с датчиком
+
+#include "config.h"
+/*
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
-#define SEALEVELPRESSURE_HPA (1013.25)                  // Задаем высоту
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+*/
+
+#define SEALEVELPRESSURE_HPA (987.92)                  // Задаем нормальное давление для 212 м - 741 мм р.с., для 0м - 1013.25
  
 Adafruit_BME280 bme;                                    // Установка связи по интерфейсу I2C
+
+float temperature, humidity, pressure, pressure_pa, altitude;
+
+ESP8266WebServer server(80);
  
 const char* ssid = WIFI_SSID;                           // Название WiFi сети
 const char* password = WIFI_PASSWORD;                   // Пароль от WiFi сети
  
-WiFiServer server(80);                                  // Указываем порт Web-сервера
-String header;
-String sTemp = "";
-String sHumi = "";
-String sPress = "";
 
+/*
 String floatToString(float x, byte precision = 2) {
   char tmp[50];
   dtostrf(x, 0, precision, tmp);
   return String(tmp);
 }
- 
-void setup() {
-  Serial.begin(115200);                                 // Скорость передачи 115200
+*/
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(100);
   bool status;
-                                                       
+                                                     
   if (!bme.begin(0x76)) {                               // Проверка инициализации датчика
     Serial.println("Could not find a valid BME280 sensor, check wiring!"); // Печать ошибки инициализации.
     while (1);                                          // Зацикливаем
   }
 
+/*
    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
 
   display.clearDisplay();
-  
-  Serial.print("Connecting to ");                       // Отправка в Serial port 
-  Serial.println(ssid);                                 // Отправка в Serial port 
-  WiFi.begin(ssid, password);                           // Подключение к WiFi Сети
-  while (WiFi.status() != WL_CONNECTED) {               // Проверка подключения к WiFi сети
-    delay(500);                                         // Пауза
-    Serial.print(".");                                  // Отправка в Serial port 
-  }
- 
-  Serial.println("");                                   // Отправка в Serial port 
-  Serial.println("WiFi connected.");                    // Отправка в Serial port 
-  Serial.println("IP address: ");                       // Отправка в Serial port 
-  Serial.println(WiFi.localIP());                       // Отправка в Serial port 
-  server.begin();
-  
   display.setTextSize(1);                               // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);                  // Draw white text                                 
+  display.setTextColor(SSD1306_WHITE);                  // Draw white text   
+*/
+  Serial.println(bme.sensorID(),16);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  // подключиться к вашей локальной wi-fi сети
+  WiFi.begin(ssid, password);
+
+  // проверить, выполнено ли подключение wi-fi сети
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
 }
- 
-void loop(){
- display.clearDisplay();
- display.setCursor(0,0); 
+void loop() 
+/*
+  display.clearDisplay();
+  display.setCursor(0,0); 
    
   float temp = bme.readTemperature();
   sTemp = "Temp: "+ floatToString(temp, 0) + " *";
@@ -79,66 +98,139 @@ void loop(){
   display.println(sHumi);
   display.println(sPress);
   display.println(WiFi.localIP());  
-  display.display();  
-   
-  WiFiClient client = server.available();               // Получаем данные, посылаемые клиентом 
- 
-  if (client) {                                         
-    Serial.println("New Client.");                      // Отправка "Новый клиент"
-    String currentLine = "";                            // Создаем строку для хранения входящих данных от клиента
-    while (client.connected()) {                        // Пока есть соединение с клиентом 
-      if (client.available()) {                         // Если клиент активен 
-        char c = client.read();                         // Считываем посылаемую информацию в переменную "с"
-        Serial.write(c);                                // Отправка в Serial port 
-        header += c;
-        if (c == '\n') {                                // Вывод HTML страницы 
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");          // Стандартный заголовок HT
-            client.println("Content-type:text/html ");
-            client.println("Connection: close");        // Соединение будет закрыто после завершения ответа
-            client.println("Refresh: 60");              // Автоматическое обновление каждые 60 сек 
-            client.println();
-            
-            client.println("<!DOCTYPE html><html>");    // Веб-страница создается с использованием HTML
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<meta charset='UTF-8'>"); 
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-                     
-            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial;}");
-            client.println("table { border-collapse: collapse; width:40%; margin-left:auto; margin-right:auto; }");
-            client.println("th { padding: 12px; background-color: #35d4c7; color: white; }");
-            client.println("tr { border: 1px solid #ddd; padding: 12px; }");
-            client.println("tr:hover { background-color: #8ad9d2; }");
-            client.println("td { border: none; padding: 12px; }");
-            client.println(".sensor { font-weight: bold; /* color:white; background-color: #bcbcbc;*/ padding: 1px; }");
-            
-            client.println("</style></head><body><h1>Метеостанция на BME280 и ESP8266</h1>");
-            client.println("<table><tr><th>Параметр</th><th>Показания</th></tr>");
-            client.println("<tr><td>Температура</td><td><span id=\"temp\" class=\"sensor\">");
-            client.println(floatToString(temp, 1));
-            client.println(" °</span></td></tr>");
-            client.println("<tr><td>Давление</td><td><span id=\"press\" class=\"sensor\">");
-            client.println(floatToString(press, 0));
-            client.println(" мм рт. ст.</span></td></tr>");
-            client.println("<tr><td>Влажность</td><td><span id=\"humi\" class=\"sensor\">");
-            client.println(floatToString(humi, 0));
-            client.println(" %</span></td></tr></table>"); 
-            client.println("</body></html>");
-            
-            client.println();
-            break;
-          } else { 
-            currentLine = "";
-          }
-        } else if (c != '\r') {  
-          currentLine += c;      
-        }
-      }
-    }
-    header = "";
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-delay(1000); 
+  display.display(); 
+
+*/
+
+{
+  server.handleClient();
+}
+
+void handle_OnConnect() 
+{
+  temperature = bme.readTemperature();
+  humidity = bme.readHumidity();
+  pressure_pa = bme.readPressure() / 100.0F;
+  pressure = pressure_pa * 0.7501;      
+  //altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  server.send(200, "text/html", SendHTML(temperature,humidity,pressure,altitude)); 
+}
+
+void handle_NotFound()
+{
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float temperature,float humidity,float pressure,float altitude)
+{
+  String ptr = "<!DOCTYPE html>";
+  ptr +="<html>";
+  ptr +="<head>";
+  ptr +="<title>ESP8266 метеостанция</title>";
+  ptr +="<meta name='viewport' content='width=device-width, initial-scale=1.0'>"; 
+  ptr +="<meta charset='UTF-8'>";    
+  ptr +="<link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet'> ";
+  
+  ptr +="<style>";
+  ptr +="html {font-family: 'Roboto', sans-serif; display: block; margin: 0px auto; text-align: center;color: #444444;}";
+  ptr +="body{margin: 0px;} ";
+  ptr +="h1 {margin: 50px auto 30px;} ";
+  ptr +=".side-by-side{display: table-cell;vertical-align: middle;position: relative;}";
+  ptr +=".text{font-weight: 600;font-size: 19px;width: 200px;}";
+  ptr +=".reading{font-weight: 300;font-size: 50px;padding-right: 25px;}";
+  ptr +=".temperature .reading{color: #F29C1F;}";
+  ptr +=".humidity .reading{color: #3B97D3;}";
+  ptr +=".pressure .reading{color: #26B99A;}";
+  ptr +=".altitude .reading{color: #955BA5;}";
+  ptr +=".superscript{font-size: 17px;font-weight: 600;position: absolute;top: 10px;}";
+  ptr +=".data{padding: 10px;}";
+  ptr +=".container{display: table;margin: 0 auto;}";
+  ptr +=".icon{width:65px}";
+  ptr +="</style>";  
+  
+  ptr +="<script>\n";
+  ptr +="setInterval(loadDoc,30000);\n";
+  ptr +="function loadDoc() {\n";
+  ptr +="var xhttp = new XMLHttpRequest();\n";
+  ptr +="xhttp.onreadystatechange = function() {\n";
+  ptr +="if (this.readyState == 4 && this.status == 200) {\n";
+  ptr +="document.body.innerHTML =this.responseText}\n";
+  ptr +="};\n";
+  ptr +="xhttp.open(\"GET\", \"/\", true);\n";
+  ptr +="xhttp.send();\n";
+  ptr +="}\n";
+  ptr +="</script>\n";  
+  ptr +="</head>";
+  
+  ptr +="<body>";
+  ptr +="<h1>ESP8266 метеостанция</h1>";
+  ptr +="<div class='container'>";
+  
+  ptr +="<div class='data temperature'>";
+  ptr +="<div class='side-by-side icon'>";
+  ptr +="<svg enable-background='new 0 0 19.438 54.003'height=54.003px id=Layer_1 version=1.1 viewBox='0 0 19.438 54.003'width=19.438px x=0px xml:space=preserve xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink y=0px><g><path d='M11.976,8.82v-2h4.084V6.063C16.06,2.715,13.345,0,9.996,0H9.313C5.965,0,3.252,2.715,3.252,6.063v30.982";
+  ptr +="C1.261,38.825,0,41.403,0,44.286c0,5.367,4.351,9.718,9.719,9.718c5.368,0,9.719-4.351,9.719-9.718";
+  ptr +="c0-2.943-1.312-5.574-3.378-7.355V18.436h-3.914v-2h3.914v-2.808h-4.084v-2h4.084V8.82H11.976z M15.302,44.833";
+  ptr +="c0,3.083-2.5,5.583-5.583,5.583s-5.583-2.5-5.583-5.583c0-2.279,1.368-4.236,3.326-5.104V24.257C7.462,23.01,8.472,22,9.719,22";
+  ptr +="s2.257,1.01,2.257,2.257V39.73C13.934,40.597,15.302,42.554,15.302,44.833z'fill=#F29C21 /></g></svg>";
+  ptr +="</div>";
+  ptr +="<div class='side-by-side text'>Температура: </div>";
+  ptr +="<div class='side-by-side reading'>";
+  ptr +=String(temperature);
+  ptr +="<span class='superscript'>&deg;C</span></div>";
+  ptr +="</div>";
+  
+  ptr +="<div class='data humidity'>";
+  ptr +="<div class='side-by-side icon'>";
+  ptr +="<svg enable-background='new 0 0 29.235 40.64'height=40.64px id=Layer_1 version=1.1 viewBox='0 0 29.235 40.64'width=29.235px x=0px xml:space=preserve xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink y=0px><path d='M14.618,0C14.618,0,0,17.95,0,26.022C0,34.096,6.544,40.64,14.618,40.64s14.617-6.544,14.617-14.617";
+  ptr +="C29.235,17.95,14.618,0,14.618,0z M13.667,37.135c-5.604,0-10.162-4.56-10.162-10.162c0-0.787,0.638-1.426,1.426-1.426";
+  ptr +="c0.787,0,1.425,0.639,1.425,1.426c0,4.031,3.28,7.312,7.311,7.312c0.787,0,1.425,0.638,1.425,1.425";
+  ptr +="C15.093,36.497,14.455,37.135,13.667,37.135z'fill=#3C97D3 /></svg>";
+  ptr +="</div>";
+  ptr +="<div class='side-by-side text'>Влажность: </div>";
+  ptr +="<div class='side-by-side reading'>";
+  ptr +=(int)humidity;
+  ptr +="<span class='superscript'>%</span></div>";
+  ptr +="</div>";
+  
+  ptr +="<div class='data pressure'>";
+  ptr +="<div class='side-by-side icon'>";
+  ptr +="<svg enable-background='new 0 0 40.542 40.541'height=40.541px id=Layer_1 version=1.1 viewBox='0 0 40.542 40.541'width=40.542px x=0px xml:space=preserve xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink y=0px><g><path d='M34.313,20.271c0-0.552,0.447-1,1-1h5.178c-0.236-4.841-2.163-9.228-5.214-12.593l-3.425,3.424";
+  ptr +="c-0.195,0.195-0.451,0.293-0.707,0.293s-0.512-0.098-0.707-0.293c-0.391-0.391-0.391-1.023,0-1.414l3.425-3.424";
+  ptr +="c-3.375-3.059-7.776-4.987-12.634-5.215c0.015,0.067,0.041,0.13,0.041,0.202v4.687c0,0.552-0.447,1-1,1s-1-0.448-1-1V0.25";
+  ptr +="c0-0.071,0.026-0.134,0.041-0.202C14.39,0.279,9.936,2.256,6.544,5.385l3.576,3.577c0.391,0.391,0.391,1.024,0,1.414";
+  ptr +="c-0.195,0.195-0.451,0.293-0.707,0.293s-0.512-0.098-0.707-0.293L5.142,6.812c-2.98,3.348-4.858,7.682-5.092,12.459h4.804";
+  ptr +="c0.552,0,1,0.448,1,1s-0.448,1-1,1H0.05c0.525,10.728,9.362,19.271,20.22,19.271c10.857,0,19.696-8.543,20.22-19.271h-5.178";
+  ptr +="C34.76,21.271,34.313,20.823,34.313,20.271z M23.084,22.037c-0.559,1.561-2.274,2.372-3.833,1.814";
+  ptr +="c-1.561-0.557-2.373-2.272-1.815-3.833c0.372-1.041,1.263-1.737,2.277-1.928L25.2,7.202L22.497,19.05";
+  ptr +="C23.196,19.843,23.464,20.973,23.084,22.037z'fill=#26B999 /></g></svg>";
+  ptr +="</div>";  
+  ptr +="<div class='side-by-side text'>Давление: </div>";
+  ptr +="<div class='side-by-side reading'>";
+  ptr +=(int)pressure;
+  ptr +="<span class='superscript'>mmHg</span></div>";
+  ptr +="</div>";
+
+/*  
+  ptr +="<div class='data altitude'>";
+  ptr +="<div class='side-by-side icon'>";
+  ptr +="<svg enable-background='new 0 0 58.422 40.639'height=40.639px id=Layer_1 version=1.1 viewBox='0 0 58.422 40.639'width=58.422px x=0px xml:space=preserve xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink y=0px><g><path d='M58.203,37.754l0.007-0.004L42.09,9.935l-0.001,0.001c-0.356-0.543-0.969-0.902-1.667-0.902";
+  ptr +="c-0.655,0-1.231,0.32-1.595,0.808l-0.011-0.007l-0.039,0.067c-0.021,0.03-0.035,0.063-0.054,0.094L22.78,37.692l0.008,0.004";
+  ptr +="c-0.149,0.28-0.242,0.594-0.242,0.934c0,1.102,0.894,1.995,1.994,1.995v0.015h31.888c1.101,0,1.994-0.893,1.994-1.994";
+  ptr +="C58.422,38.323,58.339,38.024,58.203,37.754z'fill=#955BA5 /><path d='M19.704,38.674l-0.013-0.004l13.544-23.522L25.13,1.156l-0.002,0.001C24.671,0.459,23.885,0,22.985,0";
+  ptr +="c-0.84,0-1.582,0.41-2.051,1.038l-0.016-0.01L20.87,1.114c-0.025,0.039-0.046,0.082-0.068,0.124L0.299,36.851l0.013,0.004";
+  ptr +="C0.117,37.215,0,37.62,0,38.059c0,1.412,1.147,2.565,2.565,2.565v0.015h16.989c-0.091-0.256-0.149-0.526-0.149-0.813";
+  ptr +="C19.405,39.407,19.518,39.019,19.704,38.674z'fill=#955BA5 /></g></svg>";
+  ptr +="</div>"; 
+  ptr +="<div class='side-by-side text'>Altitude</div>";
+  ptr +="<div class='side-by-side reading'>";
+  ptr +=(int)altitude;
+  ptr +="<span class='superscript'>m</span></div>";
+  ptr +="</div>";
+*/
+  
+  ptr +="</div>";
+  ptr +="</body>";
+  ptr +="</html>";
+  return ptr;
 }
