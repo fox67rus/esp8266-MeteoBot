0f44cfcf3dd5ce714a9d33c26b6c6303b7fa51c2
Elena <fox67rus@mail.ru>
2023-04-03 15:03:20 +0300

    функция для получения фаз луны и прогноза на ближайщее время.

--- src/meteo_bot/extensions.py
+++ src/meteo_bot/extensions.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 from access_config import *
 from exceptions import ConnectionException
+from configs import MOON_CODE
 
 
 def get_local_weather_data(ip=esp_ip):
@@ -60,7 +61,45 @@ def get_weather_sensitivity():
     return text
 
 
+def get_agro_forecast():
+    """
+    Прогноз для садоводов
+    """
+    url = 'https://my-calend.ru/moon-phase'
+    try:
+        page = requests.get(url).content
+    except Exception:
+        raise ConnectionException('Ошибка получения данных с сайта')
+    else:
+        soup = BeautifulSoup(page, 'lxml')
+        moon_status = []
+        table_moon_data = soup.findAll('td')
+
+        for td in table_moon_data:
+            div = td.findAll(['a', 'div'])
+            for moon_data in div:
+                if moon_data:
+                    moon_status.append(moon_data.text)
+
+        print(f'{moon_status}')
+
+        soon_moon_phase = []
+        soon_moon_date = []
+
+        for soon_phase in soup.select('.moon-phase-phases-title'):
+            soon_moon_phase.append(soon_phase.get_text())
+
+        for soon_date in soup.select('.moon-phase-phases-title ~ div'):
+            soon_moon_date.append(soon_date.get_text())
+
+        soon_moon_dict = dict(zip(soon_moon_phase, soon_moon_date))
+        print(f'{soon_moon_dict=}')
+
+
 if __name__ == '__main__':
-    print(f'{get_local_weather_data()}')
-    print(f'{get_weather_sensitivity()=}')
-    print(f'{get_weather_from_yandex()=}')
+    # print(f'{get_local_weather_data()}')
+    # print(f'{get_weather_sensitivity()=}')
+    # print(f'{get_weather_from_yandex()=}')
+    # print(f'{get_agro_forecast=}')
+    get_agro_forecast()
+
